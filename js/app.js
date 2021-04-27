99b2a66ef81f2dbd3639c41f3dced8177d67c78d
doaa-1996 <doaaobeidat1996@gmail.com>
2021-04-27 12:04:40 +0300

    edit lab3

--- js/app.js
+++ js/app.js
@@ -1,221 +1,142 @@
 'use strict';
+let selectElements = [];
+let all = [];
+let currentPage = '1';
+let currentSort = 'default';
 
-var optionOne = []; //to get page 1 ajax .
-var optionTwo = []; //to get page 1 ajax .
-
-var optionValue = []; //sort pages array by title
-var optionValueTwo = []; //sort pages array by horns
-
-var arrayImages = []; //page-1 images
-var arrayImagesTwo = []; //page-2 images
-
-
-//get page-1 images 
-function Images(title, image_url, description, horns, keyword) {
-    this.title = title;
-    this.image_url = image_url;
-    this.description = description;
-    this.horns = horns;
-    this.keyword = keyword;
-    arrayImages.push(this);
-}
-//page-2 images 
-function ImagesTwo(title, image_url, description, horns, keyword) {
-    this.title = title;
-    this.image_url = image_url;
-    this.description = description;
-    this.horns = horns;
-    this.keyword = keyword;
-    arrayImagesTwo.push(this);
-
-}
-// console.log(arrayImagesTwo);
-
-//render images 1
-Images.prototype.renderImages = function() {
-        let temp = $('#template-img').html();
-        $('.main-a').append(Mustache.render(temp, this));
-    }
-    //render images 2
-ImagesTwo.prototype.renderImages2 = function() {
-    let temp = $('#template-img').html();
-    $('.main-b').append(Mustache.render(temp, this));
-}
-
-
-Images.readJson = () => {
-    const ajaxSettings = {
-        method: 'get',
-        dataType: 'json'
-    };
-
-
-    $.ajax('data/page-1.json', ajaxSettings)
-        .then(data => {
-            data.forEach(element => {
-                optionOne.push(element.keyword);
-                let image = new Images(element.title, element.image_url, element.description, element.horns, element.keyword);
-                image.renderImages();
-            });
-            $.each(optionOne, function(i, el) {
-                if ($.inArray(el, optionValue) === -1) optionValue.push(el);
-            });
-
-            optionValue.forEach(function(value, i) {
-                $("#menu").append("<option value=" + value + ">" + value + "</option>");
-            });
-        });
+// the constructor Horn for handling data coming from the AJAX file
+const Horn = function(obj) {
+  this.title = obj.title;
+  this.url = obj.image_url;
+  this.horns = obj.horns;
+  this.description = obj.description;
+  this.keyword = obj.keyword;
+  all.push(this);
 };
 
-ImagesTwo.readJson2 = () => {
-    const ajaxSettings = {
-        method: 'get',
-        dataType: 'json'
-    };
-    $.ajax('data/page-2.json', ajaxSettings)
-        .then(data => {
-            data.forEach(element => {
-                optionTwo.push(element.keyword);
-                let image = new ImagesTwo(element.title, element.image_url, element.description, element.horns, element.keyword);
-                image.renderImages2();
-
-            });
-            $.each(optionTwo, function(i, el) {
-                if ($.inArray(el, optionValueTwo) === -1) optionValueTwo.push(el);
-            });
-
-            optionValueTwo.forEach(function(value, i) {
-                $("#menu").append("<option value=" + value + ">" + value + "</option>");
-            });
-        });
+// Prototype to render the Data one by one to the html file
+Horn.prototype.render = function () {
+  let template = $('#photo-template').html();
+  let rendering = Mustache.render(template,this);
+  $('main').append(rendering);
 };
 
-function selectKeywords() {
-    $("#menu").on('change', function() {
-        var name = this.options[this.selectedIndex].text;
-        var newPhotoTemplate2 = $('.photo-template').clone();
-        $('.main-a').html("");
-        $('.main-a').append(newPhotoTemplate2);
-        arrayImages.forEach(function(value, i) {
-            if (name === value.keyword) {
-                value.renderImages();
-            }
-        });
-    });
-}
 
-function selectKeywordsTwo() {
-    $("#menu").on('change', function() {
-        var name = this.options[this.selectedIndex].text;
-        var newPhotoTemplateTwo = $('#template-img').clone();
-        $('.main-b').html("");
-        $('.main-b').append(newPhotoTemplateTwo);
-        arrayImagesTwo.forEach(function(value, i) {
-            if (name === value.keyword) {
-                value.renderImages2();
-            }
-        });
-    });
+// Render the Date when Entering the page for the first time
+$.ajax('data/page-1.json') .then(renderData);
+
+// Render data from the AJAX file directly
+function renderData(data) {
+  selectElements = [];
+  data.forEach(obj => {
+    if(!selectElements.includes(obj.keyword)) {selectElements.push(obj.keyword);}
+    new Horn(obj);
+  });
+  renderTheAll();
+  generateTheList();
 }
 
 
-$("document").ready(function() {
-
-    Images.readJson();
-    selectKeywords();
-});
-
-var pageName = "page-1";
-$("#page-1").on("click", function() {
-
-    $("#menu").html("");
-    $(".main-b").hide();
-    $(".main-a").show();
-    $(".main-a").empty();
-    Images.readJson();
-    selectKeywords();
-    pageName = "page-1";
-
-    arrayImages = [];
-});
-
-$("#page-2").on("click", function() {
-
-    $("#menu").html("");
-    $(".main-a").hide();
-    $(".main-b").show();
-    $(".main-b").empty();
-    ImagesTwo.readJson2();
-    selectKeywordsTwo();
-    pageName = "page-2";
-    arrayImagesTwo = [];
-    // $(".photo-template2").html("");
-});
-
-$("#title").on("click", function() {
-    sortOnTitle();
-});
-$("#horns").on("click", function() {
-    sortOnHorns();
-});
-
-
-
-function sortOnTitle() {
-    if (pageName === "page-1") {
-        arrayImages.sort(function(a, b) {
-            if (a.title > b.title) return 1;
-            if (b.title > a.title) return -1;
-
-            return 0;
-        });
-
-        $('.main-a').html("");
-        arrayImages.forEach(function(value, i) {
-            value.renderImages();
-        });
-    }
-    if (pageName === "page-2") {
-        arrayImagesTwo.sort(function(a, b) {
-            if (a.title > b.title) return 1;
-            if (b.title > a.title) return -1;
-
-            return 0;
-        });
-
-        $('.main-b').html("");
-        arrayImagesTwo.forEach(function(value, i) {
-            value.renderImages2();
-        });
-    }
+// Render the "all" array, whenever called
+function renderTheAll() {
+  all.forEach(objec =>{
+    objec.render();
+  });
 }
 
-function sortOnHorns() {
+// generate the dropdown list
+function generateTheList() {
+  selectElements.forEach((keyword,idx) => {
+    let newOption = ` <option value="${keyword}" id = "${idx}">${keyword}</option>`;
+    $('select:eq(0)').append(newOption);
+  });
+}
 
-    if (pageName === "page-1") {
-        arrayImages.sort(function(a, b) {
-            if (a.horns < b.horns) return 1;
-            if (b.horns < a.horns) return -1;
+// Event listener for both the buttons, in case if the user decided to chenge the page
+$('button').on('click', function() {
+  let newPage = this.id;
+  if(newPage === currentPage) {return true;}
+  currentPage = newPage;
+  $('main').empty();
+  $('select:eq(0)').empty();
+  $('select:eq(0)').append(`<option value="default">Filter by Keyword</option>`);
+  $('select:eq(0)').prop('selectedIndex', 0).val();
+  $('select:eq(1)').prop('selectedIndex', 0).val();
+  currentSort = 'default';
+  all = [];
+  getJSONdata(newPage);
+});
 
-            return 0;
-        });
 
-        $('.main-a').html("");
-        arrayImages.forEach(function(value, i) {
-            value.renderImages();
-        });
+// change the JSON file to get data, in case of changing the page
+function getJSONdata (newPage) {
+  if(newPage === '1'){
+    $.ajax('data/page-1.json')
+      .then(renderData);
+  }
+  else if (newPage === '2'){
+    $.ajax('data/page-2.json')
+      .then(renderData);
+  }
+}
+
+// Event listener for both the "select" tags, in order to start filtering
+$('select').on('change', function () {
+  let $newValue = $('select:eq(0)').val();
+  let $newValue2 = $('select:eq(1)').val();
+  let len = $('div').length;
+  if($newValue2 !== currentSort) {
+    currentSort = $newValue2;
+    $('main').empty();
+    if($newValue2 === '1') {
+      all = sortByHorns(all);
+      renderTheAll();
     }
-    if (pageName === "page-2") {
-        arrayImagesTwo.sort(function(a, b) {
-            if (a.horns < b.horns) return 1;
-            if (b.horns < a.horns) return -1;
-
-            return 0;
-        });
-
-        $('.main-b').html("");
-        arrayImagesTwo.forEach(function(value, i) {
-            value.renderImages2();
-        });
+    else if ($newValue2 === '2') {
+      all = sortByTitle(all);
+      renderTheAll();
     }
+    else if ($newValue2 === 'default') {
+      all = [];
+      getJSONdata(currentPage);
+      $('select:eq(0)').prop('selectedIndex', 0).val();
+    }
+  }
+
+  //handling the (display: none) process
+  for(let i = 0; i < len; i++ ){
+    if ($newValue === 'default') {
+      $('div').removeClass('displays');
+    }
+    else if($(`div h5:eq(${i})`).text() === $newValue){
+      $(`div:eq(${i})`).removeClass('displays');
+    }
+    else if($(`div h5:eq(${i})`).text() !== $newValue){
+    //   console.log($(`div h5:eq(${i})`).text());
+      $(`div:eq(${i})`).addClass('displays');
+    }
+
+  }
+});
+
+// sort by Horns function
+function sortByHorns (arr) {
+  arr.sort((a,b)=> {
+    if(a.horns > b.horns)
+      return 1;
+    else if ( a.horns < b.horns )
+      return -1;
+  });
+  return arr;
+}
+
+// sort by Title function
+function sortByTitle (arr) {
+  arr.sort((a,b) => {
+    if (a.title.toUpperCase() < b.title.toUpperCase()){
+      return -1;
+    }
+    else if (a.title.toUpperCase() > b.title.toUpperCase()) return 1;
+  });
+  return arr;
 }
